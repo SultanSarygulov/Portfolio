d0851535038b9c720692a1f7b2eb6face4e966b8
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-11-02 23:48:41 +0600

    Last tweaks

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ function clicker(){
     // Change image 
     switch(score) {
         case 5:
-            document.getElementById("image").src = "style/img/abiy5.jpg";
+            document.getElementById("image").src = "img/abiy5.jpg";
             break;
         case 10:
-            document.getElementById("image").src = "style/img/abiy10.jpg";
+            document.getElementById("image").src = "img/abiy10.jpg";
             break;
         case 50:
-            document.getElementById("image").src = "style/img/abiy50.jpg";
+            document.getElementById("image").src = "img/abiy50.jpg";
             break;
         case 100:
-            document.getElementById("image").src = "style/img/abiy100.jpg";
+            document.getElementById("image").src = "img/abiy100.jpg";
             break;
     }         
 }
